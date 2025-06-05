06073f79914f5bfb88c0b8b7ea0cee2445b71b37
JekabsP1-2 <jekabs325@gmail.com>
2025-06-05 13:34:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+<div id="container">
+  <h2 id="question">Ielādē jautājumu...</h2>
+  <div class="button-group">
+    <button class="yes" onclick="nextQuestion('yes')">✅ Jā</button>
+    <button class="no" onclick="nextQuestion('no')">❌ Nē</button>
+  </div>
+</div>
